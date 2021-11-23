124bdd3c0304c44325ac107dd88acf07ac7606bf
AHMED_SAMEH <ahmedsamehnour@gmail.com>
2021-11-23 13:09:20 +0200

    material core installed/sameh

--- acl/server/index.js
+++ acl/server/index.js
@@ -12,12 +12,13 @@ app.use(bodyParser.json({limit: "20mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "20mb", extended:true}));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://javascriptmaher:maher123@cluster0.sds4o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://javascriptmaher:maher123@cluster0.sds4o.mongodb.net/Cluster0?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser:true , useUnifiedTopology:true
 }).then(() => app.listen(PORT , () => console.log(`Connection is established and running on port: ${PORT}`)))
 .catch((err) => console.log(err.message));
+
 
 //mongoose.set('useFindAndModify',false);//this shoud work but i dont know why it doesnt work for me
