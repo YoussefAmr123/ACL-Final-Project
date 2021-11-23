0524be873a77d010da26c420ceba0581f693b701
AHMED_SAMEH <ahmedsamehnour@gmail.com>
2021-11-24 01:27:22 +0200

    commit lates sameh

--- acl/client/src/components/createFlight/createFlight.js
+++ acl/client/src/components/createFlight/createFlight.js
@@ -1,8 +1,24 @@
-import * as React from 'react';
+import * as React  from 'react';
+import {useState} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
 
-export default function BasicTextFields() {
+export default function Create() {
+  const [Flight , setFlight] = useState({
+    flightNumber : 0, 
+    departureTime : 1/1/2000,
+    arrivalTime : 1/1/2000,
+    economySeats : 0,
+    businessSeats : 0 ,
+    airport : "noairport" ,
+  })
+
+  const CreateFlight = () => {
+
+  }
+
   return (
     <Box
       component="form"
@@ -12,9 +28,26 @@ export default function BasicTextFields() {
       noValidate
       autoComplete="off"
     >
-      {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" /> */}
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField id="outlined-basic" label="flight number" variant="outlined" value={Flight.flightNumber} onChange={ (event) => {
+        setFlight({...Flights, flightNumber :event.target.value})
+      }} />
+      <TextField id="outlined-basic" label="departure time" variant="outlined" value = {Flight.departureTime} onChange={ (event) => {
+        setFlight({...Flights, departureTime :event.target.value})
+      }} />
+      <TextField id="outlined-basic" label="arrival time" variant="outlined"  value = {Flight.arrivalTime} onChange={ (event) => {
+        setFlight({...Flights, arrivalTime :event.target.value})
+      }} />
+      <TextField id="outlined-basic" label="economy seats" variant="outlined"  value = {Flight.economySeats} onChange={ (event) => {
+        setFlight({...Flights, economySeats :event.target.value})
+      }} />
+      <TextField id="outlined-basic" label="business seats" variant="outlined"  value = {Flight.businessSeats} onChange={ (event) => {
+        setFlight({...Flights, businessSeats :event.target.value})
+      }} />
+      <TextField id="outlined-basic" label="airport" variant="outlined"  value = {Flight.airport} onChange={ (event) => {
+        setFlight({...Flights, airport :event.target.value})
+      }} />
+      <Button variant="contained" color="primary" onClick={CreateFlight}>
+        Create A Flight</Button>
     </Box>
   );
 }
