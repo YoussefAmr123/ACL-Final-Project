e739207d860c80fb647ad15c89cbe85bc69dad00
Youssef Amr <youssefamr4077@gmail.com>
2021-11-23 13:57:29 +0200

    userSchema/youssefamr

--- acl/server/index.js
+++ acl/server/index.js
@@ -12,11 +12,7 @@ app.use(bodyParser.json({limit: "20mb", extended:true}));
 app.use(bodyParser.urlencoded({limit: "20mb", extended:true}));
 app.use(cors());
 
-<<<<<<< Updated upstream
 const CONNECTION_URL = 'mongodb+srv://javascriptmaher:maher123@cluster0.sds4o.mongodb.net/Cluster0?retryWrites=true&w=majority';
-=======
-const CONNECTION_URL = 'mongodb+srv://Samehacl:sameh123@cluster0.sds4o.mongodb.net/Cluster0?retryWrites=true&w=majority';
->>>>>>> Stashed changes
 
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, {
