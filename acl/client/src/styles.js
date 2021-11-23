8de78d461d903591397beb8d44d72420cea9c8cc
AHMED_SAMEH <ahmedsamehnour@gmail.com>
2021-11-23 14:01:18 +0200

    till 1:41 in video/sameh

---
+++ acl/client/src/styles.js
@@ -0,0 +1,13 @@
+import {markStyles} from '@material-ui/core';
+
+export default markStyles(()=> ({
+    appBar:{
+        borderRadius : 15,
+        margin : '30px 0',
+        display : 'flex' ,
+        flexdirection : 'column',
+        justifyContent : 'center',
+        allignItems : 'center',
+     }
+
+}))
